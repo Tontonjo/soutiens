44941562d7c593124c79fd4802c43795d813bd03
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2021-04-21 22:27:14 +0200

    Update and rename mine_tent.bat to mine_ninja.cmd

---
+++ mine_ninja.cmd
@@ -0,0 +1,2 @@
+xmrig-ninja.exe -a ninja -o stratum+tcp://pool.ninjanode.net:3013 -u Ninja12PkaiWSw5EfS4rEPMNfiGm6osmx7WPtdiD3XjTLsygoQrZRCAAakRpBQX7CY5NDb72gLKayVJtai3BLn1VCskhHZRLFSD42 -p x --cpu-affinity 4 --donate-level 0
+PAUSE
--- mine_tent.bat
+++
@@ -1,2 +0,0 @@
-miner --algo 144_5 --server equihash144.eu.mine.zpool.ca:2144 --user AJewEcwBMUu9oKaeWeweawUESf9eUkzkPB --pass c=BTG --pers auto
-PAUSE
