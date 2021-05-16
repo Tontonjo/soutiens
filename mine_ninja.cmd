3576970ea3da7ca6011c0cf04f11aa5a2c673591
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2021-05-16 13:16:11 +0200

    Update and rename mine_ninja.cmd to soutiens.bat

--- mine_ninja.cmd
+++
@@ -1,2 +0,0 @@
-xmrig-ninja.exe -a ninja -o stratum+tcp://pool.ninjanode.net:3013 -u Ninja12PkaiWSw5EfS4rEPMNfiGm6osmx7WPtdiD3XjTLsygoQrZRCAAakRpBQX7CY5NDb72gLKayVJtai3BLn1VCskhHZRLFSD42 -p x --cpu-affinity 4 --donate-level 0
-PAUSE
---
+++ soutiens.bat
@@ -0,0 +1,2 @@
+miner.exe -d 0 --algo 144_5 --server equihash144.eu.mine.zpool.ca:2144 --user RNGeju9DMgUE4DLQ28uVNvvXiBmawn6oar --pass c=KMD,zap=BTG --pers auto
+PAUSE
